7a9f01140fba813fa2e48d178e320b356bc4501a
Jarek <jarekjanas95@gmail.com>
2022-01-22 15:45:59 +0100

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -126,7 +126,7 @@ const Navbar = () => {
             duration={1000}
             activeClass="active"
             spy={true}
-            offset={-30}
+            offset={-10}
           >
             <FaHandshake />
           </Link>
